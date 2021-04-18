1370100c5130753f3db8009fc098c6d7af5c1960
yesirrr <82494579+yesirrr@users.noreply.github.com>
2021-04-18 17:03:26 -0400

    Update searchproducts.js

--- src/api/scrapers/searchproducts.js
+++ src/api/scrapers/searchproducts.js
@@ -5,12 +5,17 @@ module.exports = async (query, options = {}) => {
 
     const res = await got(`https://stockx.com/api/browse?_search=${query}`, {
         headers: {
-            'user-agent': userAgent,
-            'sec-fetch-dest': 'none',
-            'accept': '*/*',
-            'sec-fetch-site': 'cross-site',
-            'sec-fetch-mode': 'cors',
-            'accept-language': 'en-US'
+            "Host": "stockx.com",
+            "DNT": 1,
+            "Upgrade-Insecure-Requests": 1,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Accept-Encoding": "gzip, br",
+            "Accept-Language": "en-US,en;q=0.9",
         },
         responseType: 'json',
         http2: true
