e7c87dd4ff425cfdeaf791b6eb54e6a2a4f881a2
yesirrr <82494579+yesirrr@users.noreply.github.com>
2021-04-18 17:03:07 -0400

    Update newsearchproducts.js

--- src/api/scrapers/newsearchproducts.js
+++ src/api/scrapers/newsearchproducts.js
@@ -5,13 +5,17 @@ module.exports = async (query, options = {}) => {
 
     const res = await got("https://xw7sbct9v6-1.algolianet.com/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.32.1&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6bfb5abee4dcd8cea8f0ca1ca085c2b3", {
         headers: {
-            "user-agent": userAgent,
-            "accept": "application/json",
-            "accept-language": "en-US,en;q=0.9",
-            "content-type": "application/x-www-form-urlencoded",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "cross-site"
+            "Host": "stockx.com",
+            "DNT": 1,
+            "Upgrade-Insecure-Requests": 1,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Accept-Encoding": "gzip, br",
+            "Accept-Language": "en-US,en;q=0.9",
         },
         body: `{\"params\":\"query=${encodeURIComponent(query)}&facets=*&filters=\"}`,
         method: "POST",
